e923c499f21356dcd90dfb98e0ea7c592456fdd5
wanghao-98 <72857652+wanghao-98@users.noreply.github.com>
2022-04-30 17:41:01 +0800

    Add files via upload

---
+++ js/time.js
@@ -0,0 +1,28 @@
+﻿function showTime() {
+	var currentDate = new Date();
+	var startDate = new Date(2022, 03, 09);
+	var date3 = currentDate - startDate;
+	var days = Math.floor(date3 / (24 * 3600 * 1000));
+	var leave1 = date3 % (24 * 3600 * 1000);     //计算天数后剩余的毫秒数
+	var hours = Math.floor(leave1 / (3600 * 1000));
+	var leave2 = leave1 % (3600 * 1000);        //计算小时数后剩余的毫秒数
+	var minutes = Math.floor(leave2 / (60 * 1000));
+	var leave3 = leave2 % (60 * 1000);          //计算分钟数后剩余的毫秒数
+	var seconds = Math.round(leave3 / 1000);
+	if (minutes < 10)
+		minutes = "0" + minutes;
+	if (seconds < 10)
+		seconds = "0" + seconds;
+	var currentTimeString = "我的刘畊宏女孩:<br>"
+		+ "I have been meet with you for:<br> "
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp"
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + days + " </c>day "
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + hours + " </c>hour "
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + minutes + " </c>min "
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + seconds + " </c>sec<br>"
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp只愿以后快快乐乐 ，相互陪伴<br>"
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
+		+ "<c style=\"color: #000; text-shadow:2px 3px 3px #FFB6C1; font-family:Microsoft YaHei; font-size:20px\" >你的小王   2022-05-01</c>";
+	document.getElementById("show").innerHTML = currentTimeString;  //改这地方
+	window.setTimeout("showTime()", 1000);
+} 
